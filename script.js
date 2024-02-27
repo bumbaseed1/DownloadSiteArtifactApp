e8536d98a676fdbe72deb3c22125616a76937e05
JackBumbaseed <jaackandersonn@gmail.com>
2024-02-27 15:44:05 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+document
+    .getElementById("downloadButton")
+    .addEventListener("click", function () {
+        // Replace 'github_username', 'repository_name', and 'file_path' with your GitHub details
+        var githubUsername = "bumbaseed1";
+        var repositoryName = "ArtifactApp";
+        var filePath = "ArtfiactApp/apk/app-debug.apk";
+
+        var fileURL = `https://raw.githubusercontent.com/${githubUsername}/${repositoryName}/main/${filePath}`;
+        var fileName = filePath.substring(filePath.lastIndexOf("/") + 1);
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", fileURL, true);
+        xhr.responseType = "blob";
+
+        xhr.onload = function () {
+            var a = document.createElement("a");
+            a.href = window.URL.createObjectURL(xhr.response);
+            a.download = fileName;
+            a.style.display = "none";
+            document.body.appendChild(a);
+            a.click();
+            delete a;
+        };
+
+        xhr.send();
+    });
